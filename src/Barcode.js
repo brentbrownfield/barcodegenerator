b9c436fbc5f2f4d887d1eec0e4f9bf2d8c626325
Brent Brownfield <bbrownfield@gmail.com>
2018-02-02 20:47:13 -0500

    Made all bwipjs options come from props with default values.

--- src/Barcode.js
+++ src/Barcode.js
@@ -7,16 +7,24 @@ class Barcode extends Component {
     }
 
     render() {
-        const {type="code128",data="UNSPECIFIED"} = this.props;
+        const {
+            type="code128",
+            data="UNSPECIFIED", 
+            scale = 3, 
+            height = 10, 
+            includeText = false, 
+            textAlign = 'center'
+        } = this.props;
+
         return (
             <canvas ref={(input) => {
                 bwipjs(input, {
-                    bcid:        type,       // Barcode type
-                    text:        data,    // Text to encode
-                    scale:       3,               // 3x scaling factor
-                    height:      10,              // Bar height, in millimeters
-                    includetext: true,            // Show human-readable text
-                    textxalign:  'center',        // Always good to set this
+                    bcid:        type,          // Barcode type
+                    text:        data,          // Text to encode
+                    scale:       scale,         // 3x scaling factor
+                    height:      height,        // Bar height, in millimeters
+                    includetext: includeText,   // Show human-readable text
+                    textxalign:  textAlign,     // Always good to set this
                     }, function (err, cvs) {
                         if (err) {
                             // Decide how to handle the error
